024c36c83a8f302cad567ef6f583e948f92ee88f
Rajashekar94 <43386967+Rajashekar94@users.noreply.github.com>
2019-04-17 12:31:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -67,28 +67,10 @@ resource "aws_autoscaling_group" "bar" {
 }
 
 ## Security Group for ELB
-resource "aws_security_group" "elb" {
-  name = "terraform-example-elb"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 ### Creating ELB
 resource "aws_elb" "example" {
   name               = "terraform-asg-example"
-  security_groups    = ["${aws_security_group.elb.id}"]
   availability_zones = ["ap-south-1a"]
 
   health_check {
